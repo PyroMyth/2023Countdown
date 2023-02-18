4adceb35e252da42f7f436876fc30ac62072f5e6
Andres Lauder <camodres@gmail.com>
2023-02-18 12:51:51 -0800

    Working on Spawn Object method
    I started working on the Spawn Objects method by accessing Resources --> SpawnableObjects folder in the game, then spawning a random object within that folder.

--- Assets/Scripts/Spawnable_Script.cs
+++ Assets/Scripts/Spawnable_Script.cs
@@ -6,33 +6,41 @@ public class Spawnable_Script : MonoBehaviour
 {
     // grab game objects trash, belongings
 
-    public GameObject trash;
-    public GameObject belonging;
+    //public GameObject trash;
+    //public GameObject belonging;
+    public Object[] prefabs; 
 
     // check trash and belongings objects with a tag
 
     // create tags around groundable objects and spawn the objects on the ground
 
-    void Start()
-    {
-        // find ground
-        GameObject spawnPointGround = GameObject.FindWithTag("Ground");
-        GameObject spawnPointTable = GameObject.FindWithTag("Table");
+void Start()
+{
+    prefabs = Resources.LoadAll("SpawnableObjects");
+    // find ground
+    GameObject spawnPointGround = GameObject.FindWithTag("Ground");
+    GameObject spawnPointTable = GameObject.FindWithTag("Table");
 
         // if spawn-point is somewhere in the game --> spawn
         if (spawnPointGround != null && spawnPointTable != null)
         {
-            // spawn game object!
-            Instantiate(trash, spawnPointGround.transform.position + new Vector3(0,10,0), Quaternion.identity);
+            //// spawn game object!
+            //Instantiate(trash, spawnPointGround.transform.position + new Vector3(0,10,0), Quaternion.identity);
 
-            Instantiate(belonging, spawnPointTable.transform.position + new Vector3(0, 10, 0), Quaternion.identity);
+            //Instantiate(belonging, spawnPointTable.transform.position + new Vector3(0, 10, 0), Quaternion.identity);
+
+            //SpawnRandom();
         }
-    }
 }
 
+//public Object SpawnRandom()
+//{
+//    foreach(Object x in prefabs)
+//        {
+//            var toSpawn = prefabs[Random.Range(0, prefabs.Length)];
+//            var spawned = Instantiate(toSpawn);
+//            return spawned;
+//        }
 
-// take in two classes of objs: trash and belongings
-
-// each class contains several differenct kinds of objs (belongings have cards or books)
-
-// pick a random gameobject to spawn in that class
+//}
+}
